8066731495c213582c229e06c9961de8db935844
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-03-17 11:43:32 -0300

    fixed socket

--- src/util/SocketInterfaces.ts
+++ src/util/SocketInterfaces.ts
@@ -12,7 +12,7 @@ interface ClientToServerEvents {
     chatMessage: (message: string, receiverId: string, senderId: string) => void; 
 }
 
-const URL = 'https://twitter-backend-production-2bd1.up.railway.app/api';
+const URL = 'https://twitter-backend-production-2bd1.up.railway.app';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL!, {
     withCredentials: true,      
